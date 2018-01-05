bc9426b88453ac60c774426f8c85cd69090f72b0
Nicolas Briemant <nicolas.briemant@gmail.com>
2018-01-05 10:39:41 +0100

    pure App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 import Missions from './components/missions';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />{' '}
-          <h1 className="App-title">Welcome to mission impossible</h1>
-        </header>
-        <Missions />
-      </div>
-    )
-  }
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />{' '}
+      <h1 className="App-title">Welcome to mission impossible</h1>
+    </header>
+    <Missions />
+  </div>
+);
 
-export default App
+export default App;
