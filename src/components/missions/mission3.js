d0cbb32f36f450c63cc69238bf42574c34b86eaf
Nicolas Briemant <nicolas.briemant@gmail.com>
2018-01-05 12:48:06 +0100

    first draft

--- src/components/missions/mission3.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const Mission3 = () => (
-  <div>name: mission3, client: client3</div>
-);
--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import missions from './data/missions2';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initialState = {
+  missions,
+};
+
+ReactDOM.render(<App {...initialState} />, document.getElementById('root'));
 registerServiceWorker();
