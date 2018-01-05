00a9efc852cc1c876bc60cb3d7c8323e52c34c4d
ale-floc <you@example.com>
2018-01-05 18:31:38 +0100

    add missions and workers list

--- src/components/missions/index.js
+++ src/components/missions/index.js
@@ -1,18 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Mission1 = () => {
-  return <h1>Mission 1</h1>;
+const Mission = ({ name, clientId, partnerId, managerId, addenda }) => {
+  return (
+    <div>
+      <h3>{name}</h3>
+      <p>
+        clientId: {clientId}, partnerId: {partnerId}, managerId: {managerId}
+      </p>
+      <p>
+        Workers ({addenda.length}): {addenda.map(w => w.workerId).join(' ')}
+      </p>
+    </div>
+  );
 };
 
-const Mission2 = () => {
-  return <h1>Mission 2</h1>;
+Mission.propTypes = {
+  name: PropTypes.string.isRequired,
+  clientId: PropTypes.string,
+  partnerId: PropTypes.string,
+  managerId: PropTypes.string,
+  addenda: PropTypes.arrayOf(
+    PropTypes.shape({
+      workerId: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
 };
 
-const Missions = () => (
-  <div>
-    <Mission1 />
-    <Mission2 />
-  </div>
-);
+const Missions = ({ missions }) => missions.map(mission => <Mission key={mission.id} {...mission} />);
+
+Missions.propTypes = {
+  missions: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+    }),
+  ).isRequired,
+};
 
 export default Missions;
---
+++ src/data/workers.js
@@ -0,0 +1,214 @@
+const workers = [
+  {
+    _id: '1',
+    email: 'eric.basley@redpelicans.com',
+    avatar: {
+      color: '#67ae3f',
+    },
+    roles: ['admin'],
+    prefix: 'Mr',
+    firstName: 'Eric',
+    lastName: 'Basley',
+    type: 'worker',
+    jobType: 'manager',
+    companyId: '1',
+    skills: ['Bootstrap', 'Docker', 'Mongodb', 'Nodejs', 'React'],
+    phones: [
+      {
+        label: 'mobile',
+        number: '06 43 80 11 16',
+      },
+    ],
+  },
+  {
+    _id: '2',
+    prefix: 'Mr',
+    firstName: 'Nicolas',
+    lastName: 'Briemant',
+    type: 'worker',
+    jobType: 'manager',
+    roles: ['admin'],
+    email: 'nicolas.briemant@redpelicans.com',
+    companyId: '1',
+    skills: [],
+    phones: [
+      {
+        label: 'mobile',
+        number: '06 33 95 22 69',
+      },
+    ],
+    avatar: {
+      color: '#ff4080',
+    },
+  },
+  {
+    _id: '3',
+    email: 'lucas.charvolin@redpelicans.com',
+    avatar: {
+      url: 'https://lh4.googleusercontent.com/-g5KIWLrszUA/AAAAAAAAAAI/AAAAAAAAAL4/K9bcZJ5Jagk/s96-c/photo.jpg',
+    },
+    roles: ['admin'],
+    prefix: 'Mr',
+    firstName: 'Lucas',
+    lastName: 'Charvolin',
+    type: 'worker',
+    jobType: 'developer',
+    companyId: '1',
+    skills: ['Bootstrap', 'Docker', 'Mongodb', 'Nodejs', 'React'],
+    phones: [
+      {
+        label: 'mobile',
+        number: '06 22 80 99 16',
+      },
+    ],
+  },
+  {
+    _id: '4',
+    email: 'mehdi.aissa-brahim@redpelicans.com',
+    avatar: {
+      color: '#67ae3f',
+    },
+    roles: ['admin'],
+    prefix: 'Mr',
+    firstName: 'Mehdi',
+    lastName: 'Aissa',
+    type: 'worker',
+    jobType: 'developer',
+    companyId: '1',
+    skills: ['Bootstrap', 'Docker', 'Mongodb', 'Nodejs', 'React'],
+    phones: [
+      {
+        label: 'mobile',
+        number: '06 11 80 99 16',
+      },
+    ],
+  },
+  {
+    _id: '5',
+    email: 'dimitri.roncoli@redpelicans.com',
+    avatar: {
+      color: '#bdc3c7',
+    },
+    roles: ['access'],
+    prefix: 'Mr',
+    firstName: 'Dimitri',
+    lastName: 'Roncoli',
+    type: 'worker',
+    jobType: 'developer',
+    companyId: '1',
+    skills: ['D3', 'Nodejs', 'React', 'Redux'],
+    phones: [
+      {
+        label: 'mobile',
+        number: '06 29 68 34 55',
+      },
+    ],
+  },
+  {
+    _id: '6',
+    email: 'marianne.porcel@redpelicans.com',
+    avatar: {
+      color: '#f1c40f',
+    },
+    roles: ['access'],
+    prefix: 'Mme',
+    firstName: 'Mariane',
+    lastName: 'Porcel',
+    type: 'worker',
+    jobType: 'developer',
+    companyId: '1',
+    skills: ['Nodejs', 'React', 'Redux'],
+    phones: [
+      {
+        label: 'mobile',
+        number: '06 21 63 42 51',
+      },
+    ],
+  },
+  {
+    _id: '7',
+    email: 'arnaud.lefloch@redpelicans.com',
+    avatar: {
+      color: '#95a5a6',
+    },
+    roles: ['access'],
+    prefix: 'Mr',
+    firstName: 'Arnaud',
+    lastName: 'LeFloch',
+    type: 'worker',
+    jobType: 'developer',
+    companyId: '1',
+    skills: ['Nodejs', 'React', 'Redux', 'StyledComponents'],
+    phones: [
+      {
+        label: 'mobile',
+        number: '06 65 63 42 31',
+      },
+    ],
+  },
+  {
+    _id: '8',
+    email: 'mike.velluet@redpelicans.com',
+    avatar: {
+      color: '#2ecc71',
+    },
+    roles: ['access'],
+    prefix: 'Mr',
+    firstName: 'Mike',
+    lastName: 'Velluet',
+    type: 'worker',
+    jobType: 'developer',
+    companyId: '1',
+    skills: ['React', 'Redux', 'StyledComponents', 'Recompose'],
+    phones: [
+      {
+        label: 'mobile',
+        number: '06 78 63 42 31',
+      },
+    ],
+  },
+  {
+    _id: '9',
+    email: 'thomas.tridon@redpelicans.com',
+    avatar: {
+      color: '#7f8c8d',
+    },
+    roles: ['access'],
+    prefix: 'Mr',
+    firstName: 'thomas',
+    lastName: 'Tridon',
+    type: 'worker',
+    jobType: 'developer',
+    companyId: '1',
+    skills: ['React', 'Popmotion', 'Redux', 'StyledComponents', 'Recompose'],
+    phones: [
+      {
+        label: 'mobile',
+        number: '06 78 63 92 31',
+      },
+    ],
+  },
+  {
+    _id: '10',
+    email: 'mahmoud.chebbou@nearteam.fr ',
+    avatar: {
+      color: '#bdc3c7',
+    },
+    roles: ['access'],
+    prefix: 'Mr',
+    firstName: 'Mahmoud',
+    lastName: 'Chebbou',
+    type: 'worker',
+    jobType: 'developer',
+    companyId: '1',
+    skills: ['React', 'Redux', 'React-Router'],
+    phones: [
+      {
+        label: 'mobile',
+        number: '09 78 82 92 31',
+      },
+    ],
+  },
+];
+
+export default workers;
