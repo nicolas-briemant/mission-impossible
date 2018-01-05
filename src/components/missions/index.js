ef6e507cb51bf1760430405573df98ca09aecf6c
Nicolas Briemant <nicolas.briemant@gmail.com>
2018-01-05 10:44:41 +0100

    play with import/export

--- src/components/missions/index.js
+++ src/components/missions/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Mission3 } from './mission3';
 
 const Mission1 = () => (
   <div>name: mission1, client: client1</div>
@@ -12,6 +13,7 @@ const Missions = () => (
   <div>
     <Mission1 />
     <Mission2 />
+    <Mission3 />
   </div>
 );
 
---
+++ src/components/missions/mission3.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Mission3 = () => (
+  <div>name: mission3, client: client3</div>
+);
