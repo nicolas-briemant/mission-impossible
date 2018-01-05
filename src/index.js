326cb67e526e89e20fabad3b119616b83773955a
Thomas TRIDON <ttridon@e3r13p1.42.fr>
2018-01-05 18:33:05 +0100

    add Mission List

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import missions from './data/missions2';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App missions={missions} />, document.getElementById('root'));
 registerServiceWorker();
