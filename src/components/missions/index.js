ea4923a299a6766d867a4be1b89011667da45471
ttridon <ttridon@student.42.fr>
2018-01-05 11:07:44 +0100

    add Mission component (#16)

---
+++ src/components/missions/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Mission1 = () => <div>name: mission1, client: client1</div>;
+
+const Mission2 = () => <div>name: mission2, client: client2</div>;
+
+const Missions = () => (
+  <div>
+    <Mission1 />
+    <Mission2 />
+  </div>
+);
+
+export default Missions;
