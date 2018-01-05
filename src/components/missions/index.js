df60b879499ea8a06e7ebc037adb9ecaa7511edd
Arnaud Le Floch <kaboffbeta@gmail.com>
2018-01-05 11:06:52 +0100

    Component missions and change app to pure function

---
+++ src/components/missions/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Mission1 = () => {
+  return <h1>Mission 1</h1>;
+};
+
+const Mission2 = () => {
+  return <h1>Mission 2</h1>;
+};
+
+const Missions = () => (
+  <div>
+    <Mission1 />
+    <Mission2 />
+  </div>
+);
+
+export default Missions;
