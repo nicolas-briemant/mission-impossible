6e773c6e2b608d7d2af13f96cfaefd8f1bb55848
MahmoudChebbo <mahmoud.chebbou@nearteam.fr>
2018-01-06 22:13:44 +0100

    add missions to App.test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,16 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { shallow } from 'enzyme'
-import App from '../App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import App from '../App';
+import missions2 from '../data/missions2';
 
 describe('App', () => {
   it('should render without crashing', () => {
-    const div = document.createElement('div')
-    ReactDOM.render(<App />, div)
-  })
+    const div = document.createElement('div');
+    ReactDOM.render(<App missions={missions2} />, div);
+  });
 
   it('should match snapshot', () => {
-    const wrapper = shallow(<App />)
-    expect(wrapper).toMatchSnapshot()
-  })
-})
+    const wrapper = shallow(<App />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
