c71b782cf0c284f4f51755dc0132f1d5609a64f8
Nicolas Briemant <nicolas.briemant@gmail.com>
2018-01-06 11:10:47 +0100

    update tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,12 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { shallow } from 'enzyme'
-import App from '../App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import App from '../App';
+import missions from '../data/missions2';
 
 describe('App', () => {
+  const initialState = {
+    missions,
+  };
+
   it('should render without crashing', () => {
     const div = document.createElement('div')
-    ReactDOM.render(<App />, div)
+    ReactDOM.render(<App {...initialState} />, div)
   })
 
   it('should match snapshot', () => {
