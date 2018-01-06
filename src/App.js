807b11560dcaf43cbd33b14ee39690fac6256fd8
Mike VELLUET <mvelluet@e3r3p20.42.fr>
2018-01-06 16:17:11 +0100

    feature mike - 3

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import logo from './logo.svg';
 import './App.css';
 import Missions from './components/missions';
 
-const App = datas => {
+const App = props => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className="App-title">Welcome to mission impossible</h1>
       </header>
-      <Missions {...datas} />
+      <Missions datas={props.missions} />
     </div>
   );
+};
+
+App.propTypes = {
+  missions: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    }),
+  ).isRequired,
 };
 export default App;
--- src/components/missions/index.js
+++ src/components/missions/index.js
@@ -2,21 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import uniqid from 'uniqid';
 
-const Missions = ({ ...datas }) => {
-  const datasCpy = datas;
+const Missions = ({ datas }) => {
+  const cpyDatas = datas;
 
-  datasCpy.missions.propTypes = {
+  cpyDatas.propTypes = {
     id: PropTypes.number.isRequired,
     name: PropTypes.string.isRequired,
     clientId: PropTypes.string.isRequired,
     partnerId: PropTypes.string.isRequired,
     managerId: PropTypes.string.isRequired,
-    addenda: PropTypes.array.isRequired,
+    addenda: PropTypes.arrayOf(
+      PropTypes.shape({
+        workerId: PropTypes.string.isRequired,
+      }),
+    ).isRequired,
   };
 
   return (
     <div>
-      {datas.missions.map(data => (
+      {datas.map(data => (
         <ul key={data.id}>
           <li>id: {data.id} </li>
           <li>name: {data.name} </li>
@@ -24,15 +28,23 @@ const Missions = ({ ...datas }) => {
           <li>partnerId: {data.partnerId}</li>
           <li>managerId: {data.managerId}</li>
           <p>addenda longueur = {data.addenda.length}</p>
-          {data.addenda.map(addenda => (
+          {data.addenda.map(worker => (
             <ul key={uniqid()}>
-              <li>{addenda.workerId}</li>
+              <li>{worker.workerId}</li>
             </ul>
           ))}
         </ul>
       ))}
     </div>
   );
+};
+
+Missions.propTypes = {
+  datas: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    }),
+  ).isRequired,
 };
 
 export default Missions;
