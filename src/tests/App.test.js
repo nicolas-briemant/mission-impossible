b2ff3c4bd3ad89c6e35c6da6f9e62eaedd20ef4c
Mike VELLUET <mvelluet@e3r3p20.42.fr>
2018-01-06 16:50:49 +0100

    feature mike - 3

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -6,11 +6,11 @@ import App from '../App';
 describe('App', () => {
   it('should render without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    ReactDOM.render(<App missions={[]} />, div);
   });
 
   it('should match snapshot', () => {
-    const wrapper = shallow(<App />);
+    const wrapper = shallow(<App missions={[]} />);
     expect(wrapper).toMatchSnapshot();
   });
 });
