47a31c598197a4aea7c067d08ee69b6f6d2d3fa4
Nicolas Briemant <nicolas-briemant@users.noreply.github.com>
2018-01-06 11:22:40 +0100

    dev2 update dev1 with props, PropTypes and data from a store (#25)
    * mv data/
    
    * first draft
    
    * update tests

--- src/components/missions/index.js
+++ src/components/missions/index.js
@@ -1,20 +1,38 @@
-import React from 'react';
-import { Mission3 } from './mission3';
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
 
-const Mission1 = () => (
-  <div>name: mission1, client: client1</div>
-);
-
-const Mission2 = () => (
-  <div>name: mission2, client: client2</div>
-);
-
-const Missions = () => (
+const Mission = ({name, clientId, partnerId, managerId, addenda}) => (
   <div>
-    <Mission1 />
-    <Mission2 />
-    <Mission3 />
+    <h2>{name}</h2>
+    <em>{[clientId, partnerId, managerId].join(' - ')}</em>
+    <div>addenda: (#{addenda.length}) {addenda.map((a) => a.workerId).join(' - ')}</div>
   </div>
 );
+
+Mission.propTypes = {
+  name: PropTypes.string.isRequired,
+  clientId: PropTypes.string,
+  partnerId: PropTypes.string,
+  managerId: PropTypes.string,
+  addenda: PropTypes.arrayOf(
+    PropTypes.shape({
+      workerId: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+
+const Missions = ({ missions }) => (
+  <Fragment>
+    {missions.map((mission) => <Mission key={mission._id} {...mission} />)}
+  </Fragment>
+);
+
+Missions.propTypes = {
+  missions: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.number,
+    })
+  ).isRequired,
+};
 
 export default Missions;
