5cc2057a5670ec28d24695032743530c61e513da
MahmoudChebbo <33488803+MahmoudChebbo@users.noreply.github.com>
2018-01-08 13:50:30 +0100

    Fix Test App (#27)
    * update readme
    
    * Add PropTypes and List Missions
    
    * add missions to App.test
    
    * fixed test
    
    * fixed test-second try
    
    * index.js fixed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import logo from './logo.svg';
 import './App.css';
 import Missions from './components/missions/index';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" /> <h1 className="App-title">Welcome to mission impossible</h1>
-      </header>
-      <Missions />
-    </div>
-  );
-}
+const App = ({ missions }) => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" /> <h1 className="App-title">Welcome to mission impossible</h1>
+    </header>
+    <Missions missions={missions} />
+  </div>
+);
+
+App.propTypes = {
+  missions: PropTypes.array,
+};
 
 export default App;
--- src/components/missions/index.js
+++ src/components/missions/index.js
@@ -1,14 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Mission1 = () => <div>Mission: mission 1 ,Client: client 1 </div>;
+const Mission = ({ name, clientId, partnerId, managerId, addenda }) => {
+  return (
+    <li>
+      <h3>Name: {name}</h3>
+      <p>
+        clientId: {clientId}, partnerId: {partnerId}, managerId: {managerId}
+      </p>
+      <p>
+        Workers ({addenda.length}): {addenda.map(worker => worker.workerId)}
+      </p>
+    </li>
+  );
+};
 
-const Mission2 = () => <div>Mission: mission 2 ,Client: client 2 </div>;
+Mission.propTypes = {
+  name: PropTypes.string.isRequired,
+  clientId: PropTypes.string,
+  partnerId: PropTypes.string,
+  managerId: PropTypes.string,
+  addenda: PropTypes.arrayOf(
+    PropTypes.shape({
+      workerId: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
 
-const Missions = () => (
-  <div>
-    {Mission1}
-    {Mission2}
-  </div>
-);
+const Missions = ({ missions }) => <ul>{missions.map(mission => <Mission key={mission.id} {...mission} />)}</ul>;
+
+Missions.propTypes = {
+  missions: PropTypes.array,
+};
 
 export default Missions;
