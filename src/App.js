9feaf1af6d45c650bd6dd4d85288765f0849f56f
Arnaud Le Floch <kaboffbeta@gmail.com>
2018-01-08 10:25:50 +0100

    add missions and workers list (#23)
    * add missions and workers list
    
    * add new test
    
    * add snap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import logo from './logo.svg';
 import './App.css';
 import Missions from './components/missions';
 
-const App = () => (
+const App = ({ missions }) => (
   <div className="App">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" /> <h1 className="App-title">Welcome to mission impossible</h1>
     </header>
-    <Missions />
+    <Missions missions={missions} />
   </div>
 );
+
+App.propTypes = {
+  missions: PropTypes.array,
+};
 
 export default App;
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { shallow } from 'enzyme'
-import App from '../App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import App from '../App';
 
 describe('App', () => {
   it('should render without crashing', () => {
-    const div = document.createElement('div')
-    ReactDOM.render(<App />, div)
-  })
+    const div = document.createElement('div');
+    ReactDOM.render(<App missions={[]} />, div);
+  });
 
   it('should match snapshot', () => {
-    const wrapper = shallow(<App />)
-    expect(wrapper).toMatchSnapshot()
-  })
-})
+    const wrapper = shallow(<App missions={[]} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
