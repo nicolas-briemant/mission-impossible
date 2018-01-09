7c6ab3701739c7b5557c625f31d3f3656fdb3219
ale-floc <you@example.com>
2018-01-09 11:40:22 +0100

    add classname and proptypes children

--- src/components/app/logo.js
+++ src/components/app/logo.js
@@ -1,3 +1,10 @@
 import React from 'react';
+import classNames from 'classnames';
 
-export default () => <img src='https://pbs.twimg.com/profile_images/576032932810227712/V-AdqQ_j_400x400.png' className="imgheader" alt="logo" />
+export default () => (
+  <img
+    src="https://pbs.twimg.com/profile_images/576032932810227712/V-AdqQ_j_400x400.png"
+    className={classNames('imgheader')}
+    alt="logo"
+  />
+);
--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import glamorous from 'glamorous';
-import 'normalize.css/normalize.css'
-import '@blueprintjs/core/dist/blueprint.css'
+import 'normalize.css/normalize.css';
+import '@blueprintjs/core/dist/blueprint.css';
 
 const DivHeader = glamorous.header({
-	background: '#1a2c51',
-	display: 'flex',
-	justifyContent: 'space-between',
-	margin: '2rem 2rem 0rem 2rem',
-	alignItems: 'center',
-	padding: '1rem'
-})
+  background: '#1a2c51',
+  display: 'flex',
+  justifyContent: 'space-between',
+  margin: '2rem 2rem 0rem 2rem',
+  alignItems: 'center',
+  padding: '1rem',
+});
 
-export const HeaderLeft = ({ children }) => children
-export const HeaderRight = ({ children }) => children
-export const HeaderCenter = ({ children }) => children
-
+export const HeaderLeft = ({ children }) => children;
+export const HeaderRight = ({ children }) => children;
+export const HeaderCenter = ({ children }) => children;
 
 export const Header = ({ children }) => {
+  const headerLeft = () => React.Children.toArray(children).find(({ type }) => type === HeaderLeft);
+  const headerCenter = () => React.Children.toArray(children).find(({ type }) => type === HeaderCenter);
+  const headerRight = () => React.Children.toArray(children).find(({ type }) => type === HeaderRight);
 
-	const headerLeft = () => React.Children.toArray(children).find(({type}) => type === HeaderLeft)
-	const headerCenter = () => React.Children.toArray(children).find(({type}) => type === HeaderCenter)
-	const headerRight = () => React.Children.toArray(children).find(({type}) => type === HeaderRight)
+  return (
+    <DivHeader>
+      <div>{headerLeft()}</div>
+      <div>{headerCenter()}</div>
+      <div>{headerRight()}</div>
+    </DivHeader>
+  );
+};
 
-	return (
-		<DivHeader>
-			<div>{headerLeft()}</div>
-			<div>{headerCenter()}</div>
-			<div>{headerRight()}</div>
-		</DivHeader>
-	)
-	
-}
+Header.propTypes = {
+  children: PropTypes.node,
+};
