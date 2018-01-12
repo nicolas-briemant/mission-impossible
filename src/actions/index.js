3e9aab929f829a87e3d4abf39cbd2ce61878f494
Nicolas Briemant <nicolas.briemant@gmail.com>
2018-01-12 12:16:59 +0100

    test actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,8 @@
 export const REMOVE_MISSION = 'REMOVE_MISSION';
-export const REMOVE_MISSIONS = 'REMOVE_MISSIONS';
-export const TOGGLE_MISSION = 'TOGGLE_MISSION';
-
-// action
-//export const removeMission = { type: REMOVE_MISSION, payload: { missionId: 1 } };
-
-// action creator
 export const removeMission = (missionId) => ({ type: REMOVE_MISSION, payload: { missionId } });
+
+export const REMOVE_MISSIONS = 'REMOVE_MISSIONS';
 export const removeMissions = () => ({ type: REMOVE_MISSIONS });
+
+export const TOGGLE_MISSION = 'TOGGLE_MISSION';
 export const toggleMission = (missionId) => ({ type: TOGGLE_MISSION, payload: { missionId } });
---
+++ src/actions/tests/index.test.js
@@ -0,0 +1,18 @@
+import * as actions from '..';
+
+describe('actions', () => {
+  it('should create an action to remove a mission', () => {
+    const expectedAction = { type: 'REMOVE_MISSION', payload: { missionId: 1 } };
+    expect(actions.removeMission(1)).toEqual(expectedAction);
+  });
+
+  it('should create an action to remove missions', () => {
+    const expectedAction = { type: 'REMOVE_MISSIONS' };
+    expect(actions.removeMissions()).toEqual(expectedAction);
+  });
+
+  it('should create an action to toggle a mission', () => {
+    const expectedAction = { type: 'TOGGLE_MISSION', payload: { missionId: 1 } };
+    expect(actions.toggleMission(1)).toEqual(expectedAction);
+  });
+});
