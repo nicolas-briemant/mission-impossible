f753a651572bb8120fe8fa974798bfbc0df345fd
Nicolas Briemant <nicolas.briemant@gmail.com>
2018-01-15 18:20:23 +0100

    update tests

--- src/tests/header.test.js
+++ src/tests/header.test.js
@@ -2,38 +2,37 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 import { Header, HeaderLeft, HeaderRight } from '../components/app/header';
 
-describe('src | components | header', () => {
-  describe('render', () => {
-    test('should match snapchot', () => {
-      const wrapper = shallow(<Header />);
-      expect(wrapper).toMatchSnapshot();
-    });
+describe('Header', () => {
+  it('should match snapchot', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper).toMatchSnapshot();
+  });
 
-    test('should match snapchot with children', () => {
-      const root = (
-        <Header>
-          <HeaderLeft>left</HeaderLeft>
-          <HeaderRight>right</HeaderRight>
-        </Header>
-      );
-      const wrapper = mount(root);
-      expect(wrapper.find(HeaderLeft)).toHaveLength(1);
-    });
+  it('should match snapchot with children', () => {
+    const root = (
+      <Header>
+        <HeaderLeft>
+          <span>left</span>
+        </HeaderLeft>
+      </Header>
+    );
+    const wrapper = mount(root);
+    expect(wrapper.find(HeaderLeft)).toHaveLength(1);
+  });
 
-    test('should match snapchot with children text', () => {
-      const root = (
-        <Header>
-          <HeaderLeft>
-            <span>left</span>
-          </HeaderLeft>
-          <HeaderRight>
-            <span>right</span>
-          </HeaderRight>
-        </Header>
-      );
-      const wrapper = mount(root);
-      const expectedElements = [<span>left</span>, <span>right</span>];
-      expect(wrapper.containsAllMatchingElements(expectedElements)).toEqual(true);
-    });
+  it('should match snapchot with children text', () => {
+    const root = (
+      <Header>
+        <HeaderLeft>
+          <span>left</span>
+        </HeaderLeft>
+        <HeaderRight>
+          <span>right</span>
+        </HeaderRight>
+      </Header>
+    );
+    const wrapper = mount(root);
+    const expectedElements = [<span>left</span>, <span>right</span>];
+    expect(wrapper.containsAllMatchingElements(expectedElements)).toEqual(true);
   });
 });
