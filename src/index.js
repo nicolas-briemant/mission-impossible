4c80a35edaf279d2e1f4a1b0cbb87cc77a601e02
Nicolas Briemant <nicolas-briemant@users.noreply.github.com>
2018-01-18 09:17:26 +0100

    dev10 redux migration (#47)

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import missions from './data/missions2';
 import reducer from './reducers';
-import Provider from './provider';
-import { createStore } from './store';
 
 const initialState = {
   missions,
   people: [],
 };
 
-const store = createStore(reducer, initialState);
+const store = createStore(reducer, initialState, applyMiddleware(logger));
 
 ReactDOM.render(
   <Provider store={store}>
